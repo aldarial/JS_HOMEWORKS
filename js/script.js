927c50f654c6619184e895e07becda93113d986d
aldarial <dariapersikk@gmail.com>
2024-02-25 17:12:15 +0100

    hw6

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 'use strict'
 
 
-let line1 = prompt('Enter the first line:');
-let line2 = prompt('Enter the second line:');
-let line3 = prompt('Enter the third line:');
-
-let allLines= line1 + " " + line2 + " " + line3;
-
-console.log(allLines);
-
-
-
-
-// const
-//     num = 123456,
-//     arr = ('' + num).split('')
+// let line1 = prompt('Enter the first line:');
+// let line2 = prompt('Enter the second line:');
+// let line3 = prompt('Enter the third line:');
 //
-// console.log(arr.map(Number))
+// let allLines= line1 + " " + line2 + " " + line3;
+//
+// console.log(allLines);
+
+
+
+
+
+const
+    num = 12345,
+    arr = ('' + num).split('')
+
+console.log(arr.map(Number))
