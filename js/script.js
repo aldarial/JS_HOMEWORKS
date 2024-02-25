9c50cd92cc51aaad3a478208c26ea5bcbe3792ea
aldarial <dariapersikk@gmail.com>
2024-02-25 15:18:00 +0100

    hw7

---
+++ js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// /*Task 1*/
+
+let admin;
+
+const name = "John";
+admin = name;
+
+
+alert(admin);
+
+
+
+/*Task 2*/
+
+// const planet = 'Earth';
+// const user = 'Name';
+
+
+
+/*Task 3*/
+
+// let variable = "10" + 10;
+//
+// console.log(typeof variable);
