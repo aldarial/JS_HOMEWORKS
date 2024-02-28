abfc780e4a643423d86e4002a45180739932e330
aldarial <dariapersikk@gmail.com>
2024-02-28 21:38:46 +0100

    hw8 corrected

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 'use strict'
 
-let age= prompt('How old are you?');
-let number = Number(age);
+const age= prompt('How old are you?');
+const number = Number(age);
 
-let futureAge = number + 10;
+const futureAge = number + 10;
 
 alert('In 10 years you will be' + ' ' + futureAge + ' ' + 'years old.');
