3e870405f2a14131099ddf3b078fe2bc8a434eb6
aldarial <dariapersikk@gmail.com>
2024-02-28 21:36:43 +0100

    hw6 corrected

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 'use strict'
 
 /*#1*/
-let line1 = prompt('Enter the first line:');
-let line2 = prompt('Enter the second line:');
-let line3 = prompt('Enter the third line:');
+const line1 = prompt('Enter the first line:');
+const line2 = prompt('Enter the second line:');
+const line3 = prompt('Enter the third line:');
 
-let allLines= line1 + " " + line2 + " " + line3;
+const allLines= line1 + " " + line2 + " " + line3;
 
 console.log(allLines);
 
@@ -13,21 +13,7 @@ console.log(allLines);
 
 /* #2*/
 
-// const
-//     num = 12345,
-//     arr = ('' + num).split('')
-//
-// console.log(arr.map(Number))
-
-/*or*/
-
-// let str = String.raw`12345`;
-//
-// console.log(str.split("").join(" "));
-
-/*or*/
-
-// let num = 42378;
-// let result = num.toString().split('').join(' ');
-// console.log(result);
+const num = 42378;
+const result = num.toString().split('').join(' ');
+console.log(result);
 
